e23077d2779fcbb23b1163e5aafb132debd10dbc
doug peterson <dougpeterson@dougs-MacBook-Pro.local>
2022-01-19 20:00:42 -0600

    Added new project

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,54 +1,55 @@
 // --------------------------------------- Project List
 export const ProjectList = [
-    {
-      img: "https://user-images.githubusercontent.com/85598391/138613712-3cb3d1f3-4388-44fc-a0f6-6c17264c6a6b.gif",
-      title: "Note Taker",
-      description: "This is an application that can be used to write and save notes. This application uses an Express.js back end and saves and retrieves note data from a JSON file.",
-      tech_stack: "JavaScript, Node.js, Express.js, HTML, CSS",
-      github_url: "https://github.com/Doug-Coder64/Note-Taker",
-      demo_url: "https://young-woodland-88281.herokuapp.com/",
-    },
-    {
-      img: "https://user-images.githubusercontent.com/85598391/144765458-e8f60c4e-1081-4472-97d5-40a1e7f45037.gif",
-      title: "E-commerce Back End",
-      description: "A back end for an e-commerce website that uses Express.js, Sequalize and MySQL",
-      tech_stack: "JavaScript, Node.js, Express.js, MySQL, HTML, CSS",
-      github_url: "https://github.com/Doug-Coder64/E-commerce-Back-End",
-      demo_url: "https://github.com/Doug-Coder64/E-commerce-Back-End",
-    },
-    {
-      img: "https://raw.githubusercontent.com/Doug-Coder64/password-generator/main/assets/readme-images/example.png",
-      title: "Password Generator",
-      description: "This is an application that an employee can use to generate a random password based on criteria they’ve selected.",
-      tech_stack: "JavaScript, HTML, CSS",
-      github_url: "https://github.com/Doug-Coder64/password-generator",
-      demo_url: "https://doug-coder64.github.io/password-generator/",
-    },
-    
-  ];
-  
-  // --------------------------------------- Skills
-  
-  export const stackList = [
-    {
-      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
-      name: "HTML",
-    },
-    {
-      img: "https://raw.githubusercontent.com/voodootikigod/logo.js/1544bdeed6d618a6cfe4f0650d04ab8d9cfa76d9/js.svg",
-      name: "JavaScript"
-    },
-    {
-      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/react.svg",
-      name: "ReactJS",
-    },
-    {
-      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/bootstrap.svg",
-      name: "Bootstrap",
-    },
-    {
-      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
-      name: "Git",
-    },
+  {
+    img: "https://user-images.githubusercontent.com/85598391/138613712-3cb3d1f3-4388-44fc-a0f6-6c17264c6a6b.gif",
+    title: "Note Taker",
+    description:
+      "This is an application that can be used to write and save notes. This application uses an Express.js back end and saves and retrieves note data from a JSON file.",
+    tech_stack: "JavaScript, Node.js, Express.js, HTML, CSS",
+    github_url: "https://github.com/Doug-Coder64/Note-Taker",
+    demo_url: "https://young-woodland-88281.herokuapp.com/",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/85598391/144765458-e8f60c4e-1081-4472-97d5-40a1e7f45037.gif",
+    title: "E-commerce Back End",
+    description:
+      "A back end for an e-commerce website that uses Express.js, Sequalize and MySQL",
+    tech_stack: "JavaScript, Node.js, Express.js, MySQL, HTML, CSS",
+    github_url: "https://github.com/Doug-Coder64/E-commerce-Back-End",
+    demo_url: "https://github.com/Doug-Coder64/E-commerce-Back-End",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/85598391/150241289-6aaee5ad-d436-4a11-bb05-4b188ebe0a79.png",
+    title: "Pampered Pantry",
+    description:
+      "This is an application that allows users to add and share their recipes.",
+    tech_stack: "MongoDB, Express, GraphQL, JavaScript, HTML, CSS",
+    github_url: "https://github.com/DarrickPrichett/Pampered_Pantry",
+    demo_url: "https://pampered-pantry.herokuapp.com/",
+  },
+];
 
-  ];
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/voodootikigod/logo.js/1544bdeed6d618a6cfe4f0650d04ab8d9cfa76d9/js.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/react.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/bootstrap.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
+    name: "Git",
+  },
+];
